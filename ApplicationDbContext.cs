13f7043b010c9d868b85c2a2803b11e0276be685
supersecretusername <ds>
2025-02-18 15:35:16 -0500

    setup migrations and added Microsoft.AspNetCore.Identity so we have basic user auth tools

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using SurveyTool.Models;
+using SurveyTool.Models; // Ensure that ApplicationUser is defined in this namespace
 
 namespace SurveyTool
 {
-  public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
   {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SurveyTool.Models
+{
+  public class ApplicationUser : IdentityUser
+  {
+    // Add additional properties if needed
+  }
+}
